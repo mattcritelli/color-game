128138fcfb6585b6ba8688ba158de22f8a0b19c0
mattcritelli <matthew.critelli@gmail.com>
2017-08-15 14:04:37 -0600

    Add additional game funcitonality.  Adds proper winner/loser class name to tile.  Removes incorrect tiles selected.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+var numTiles = 6;
 var allColors = [];
-var winningColor = selectWinningColor();
+var winningColor;
+var headline = document.getElementById("winning-color");
 var tiles = document.querySelectorAll(".tile");
+var tileBoard = document.querySelector(".tile-board");
 var reset = document.querySelector("#reset").addEventListener("click", newGame);
 
+tileBoard.addEventListener("click", function(e){
+  if(e.target.classList[1] === "loser"){
+    console.log("loser clicked")
+    e.target.classList.add("hide-tile");
+  } else {
+    console.log("winner");
+  }
+})
 
 function randomColor(){
   var colors = [];
@@ -12,22 +23,47 @@ function randomColor(){
   return "rgb(" + colors[0] +', ' + colors[1] + ', ' + colors[2] +")"
 }
 
-function generateTiles(){
+function generateColorArray(){
   for(var i = 0; i < 6; i++){
     allColors.push(randomColor());
   }
 }
 
+function generateTiles(){
+  for(var i = 0; i < tiles.length; i++){
+    tiles[i].style.backgroundColor = allColors[i];
+  }
+}
+
+function resetWinnerClass(){
+  for(var i = 0; i < tiles.length; i++){
+    tiles[i].classList.remove("winner");
+  }
+}
+
 function selectWinningColor(){
-  return Math.floor(Math.random()*7)
+  return Math.floor(Math.random()*(numTiles))
+}
+
+function addClassToTiles(){
+  for(var i = 0; i < tiles.length; i++){
+    if(i === winningColor){
+      tiles[i].classList.add("winner");
+    } else {
+      tiles[i].classList.add("loser");
+    }
+  }
 }
 
 function newGame(){
+  winningColor = selectWinningColor();
   allColors = [];
+  resetWinnerClass();
+  generateColorArray();
   generateTiles();
-  for(var i = 0; i < tiles.length; i++){
-    tiles[i].style.background = allColors[i];
-  }
+  addClassToTiles();
+  headline.innerText = allColors[winningColor];
+  // tiles[winningColor].classList.add("winner");
 }
 
 newGame();
